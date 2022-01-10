b1fde96bb49dcf8c7130cf54aa329beba21546a6
mehmetdemircioglu <demircioglumhmt@gmail.com>
2022-01-10 21:41:23 +0300

    create redux define, dummy data and productoverview edit

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { FlatList, Text } from "react-native";
+import { useSelector } from "react-redux";
+
+const ProductOverviewScreen = props => {
+
+    const products = useSelector(state => state.products.avaiableProducts)
+
+    return <FlatList
+        data={products}
+        keyExtractor={item => item.id}
+        renderItem={
+            itemData => <Text>{itemData.item.title
+            }</Text>
+        } />
+}
+
+export default ProductOverviewScreen;
